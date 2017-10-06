7708dd4d215f030ccb83cfae5d6d5b3be79806ef
daniel-enere <daniel.enere@gmail.com>
2017-10-05 23:15:49 -0500

    debugged

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-};
-mongoose.connect(db.localhost || process.env.MONGO_URI);
+
+mongoose.connect('mongodb://localhost:27017/TodoApp' || process.env.MONGO_URI);
 
 module.exports = {mongoose};
