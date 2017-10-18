c483217640e8e0ef020ee990177c6c70ce03dd34
daniel-enere <daniel.enere@gmail.com>
2017-10-17 21:53:57 -0500

    patch added to Todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
+// const MongoClient = require('mongodb').MongoClient
+//   , assert = require('assert');
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp');
 
+// // Connection URL
+// var url = 'mongodb://localhost:27017/myproject';
+// // Use connect method to connect to the Server
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
+//
+//   db.close();
+// });
+
 module.exports = {mongoose};
+// {MongoClient}
