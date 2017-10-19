f12beeadc61ceddf35cf13d115b5e2f5f67ae878
daniel-enere <daniel.enere@gmail.com>
2017-10-19 12:17:09 -0500

    Hased pass before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = 'qwert123';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, "123abc");
-console.log('*******', decoded, '**************');
+var hashedPass = '$2a$10$NEoq1XwQN3gyitIpzVA/pe4IIrlscZ3NsfnAr9KvByE6xWU1xFec.';
+
+bcrypt.compare(password, hashedPass, (err, res) => {
+  console.log(res);
+})
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, "123abc");
+// console.log('*******', decoded, '**************');
 
 // var message = "I am user number 3";
 // var hash = SHA256(message).toString();
