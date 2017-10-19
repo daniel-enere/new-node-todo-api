e78c0348e5b27fda14f159ebe0b4ee3f5312046d
daniel-enere <daniel.enere@gmail.com>
2017-10-19 10:04:25 -0500

    POST /users route and token auths

--- server/server.js
+++ server/server.js
@@ -14,18 +14,33 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/users',(req, res) => {
-  var user = new User({
-    email: req.body.email
-  });
+//Users
+// app.post('/users',(req, res) => {
+//   var user = new User({
+//     email: req.body.email
+//   });
+//
+//   user.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//     res.status(400).send(e);
+//   })
+// });
 
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return  user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
+//Todos
 app.post('/todos',(req, res) => {
   var todo = new Todo({
     text: req.body.text
