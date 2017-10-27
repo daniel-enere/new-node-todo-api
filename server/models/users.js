5e2fa24fb4748291e245f2f1b97be22fa361e211
daniel-enere <daniel.enere@gmail.com>
2017-10-27 10:35:57 -0500

    added DELETE /users/me/token

--- server/models/users.js
+++ server/models/users.js
@@ -54,6 +54,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findbyToken = function (token) {
   var User = this;
   var decoded;
